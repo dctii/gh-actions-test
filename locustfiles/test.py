05f9f13eb9807fa0c602789ffb79b092e497b5a3
dctii <danactolman@gmail.com>
2023-07-25 21:43:11 -0700

    Add locustfile and requirements.txt

---
+++ locustfiles/test.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+
+from locust.env import Environment
+from locust.user.users import HttpUser
+from locust.user.task import task, TaskSet
+from locust.user.wait_time import between
+
+load_dotenv()
+
+class BaseTaskSet(TaskSet):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.headers = self.user.client.headers
+
+
+class MyTest(BaseTaskSet):
+    def on_start(self):
+        pass
+
+    @task
+    def load_test(self):
+        endpoint = '/'
+
+        with self.client.get(
+                endpoint,
+                timeout=10,
+                headers=self.headers,
+                catch_response=True) as response:
+            if response.status_code is 200:
+                print(f"{response.status_code} GOOD REQUEST")
+                response.success()
+            else:
+                response.failure(f"{response.status_code}: BAD REQUEST")
+
+    def on_stop(self):
+        pass
+
+
+class DesktopUser(HttpUser):
+    wait_time = between(1, 12)
+    host = os.getenv("HOST")
+    tasks = [MyTest]
+
+
+class MyEnvironment(Environment):
+    user_classes = [DesktopUser]
+
+
+environment = MyEnvironment()
