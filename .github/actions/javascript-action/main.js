35e6ad27e92ee00c41d4d0075fe49e549ae27102
dctii <danactolman@gmail.com>
2023-08-16 15:09:22 -0700

    add setup.js to js action workflow

--- .github/actions/javascript-action/main.js
+++ .github/actions/javascript-action/main.js
@@ -1,7 +1,33 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
-function run() {
-    core.notice('Hello from my custom JavaScript Action!')
+const { exec } = require('child_process');
+
+function installDependencies() {
+    return new Promise((resolve, reject) => {
+        exec('npm install @actions/core @actions/github @actions/exec', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                reject(error);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+            }
+            resolve();
+        });
+    });
 }
+
+async function run() {
+    try {
+        await installDependencies();
+
+        // Now that the dependencies are installed, you can require them:
+        const core = require('@actions/core');
+
+        core.notice('Hello from my custom JavaScript Action!');
+    } catch (error) {
+        console.error(`Failed: ${error}`);
+    }
+}
+
 run();
