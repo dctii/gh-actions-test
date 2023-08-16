40aeebfa06f76a2b9d36dab7414f8788c8d5f32d
dctii <danactolman@gmail.com>
2023-08-16 15:38:30 -0700

    add setup.js to js action workflow

--- .github/actions/javascript-action/main.js
+++ .github/actions/javascript-action/main.js
@@ -1,21 +1,4 @@
-const { exec } = require('child_process');
-
-function installDependencies() {
-    return new Promise((resolve, reject) => {
-        exec('npm install @actions/core @actions/github @actions/exec', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                reject(error);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-            }
-            resolve();
-        });
-    });
-}
+const { installDependencies } = require('./setup');
 
 async function run() {
     try {
--- .github/actions/javascript-action/setup.js
+++ .github/actions/javascript-action/setup.js
@@ -1,17 +1,27 @@
-const { exec} = require('child_process');
+const { exec } = require('child_process');
 
-exec(
-    'npm install @actions/core @actions/github @actions/exec',
-    (
-        error,
-        stdout,
-        stderr
-    ) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        process.exit(1);
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-});
+function installDependencies() {
+    return new Promise((resolve, reject) => {
+        exec('npm install @actions/core @actions/github @actions/exec',
+            (
+                error,
+                stdout,
+                stderr
+            ) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                reject(error);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+            }
+            resolve();
+        });
+    });
+}
 
+module.exports = {
+    installDependencies
+};
