713e99f13a53f1831d609c060164736c550884ee
dctii <danactolman@gmail.com>
2023-08-17 11:20:29 -0700

    updates, add docker-action test

--- .github/actions/docker-action/test.py
+++ .github/actions/docker-action/test.py
@@ -7,6 +7,10 @@ def kc(key):
     return "INPUT_" + key.upper()
 
 
+def set_output(key, val, file):
+    print(f"{key}={val}", file=file)
+
+
 def run():
     p = inflect.engine()
     first_name = os.environ[kc("first-name")]
@@ -17,7 +21,7 @@ def run():
     last_name = "Tolman"
 
     with open(os.environ["GITHUB_OUTPUT"], "a") as gh_output:
-        print(f"last-name={last_name}", file=gh_output)
+        set_output(key="last-name", val=last_name, file=gh_output)
 
 
 if __name__ == "__main__":
